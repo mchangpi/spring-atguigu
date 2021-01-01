ad374d978694e909575b7ec55d09a53b1a9a44c9
Milton Chang <mchangpi@gmail.com>
2021-01-01 18:45:58 +0800

    P22. Annotation Config - @Autowired @Qualifier

--- src/com/miltonlearn/service/UserService.java
+++ src/com/miltonlearn/service/UserService.java
@@ -2,16 +2,20 @@ package com.miltonlearn.service;
 
 import com.miltonlearn.call.IUserCall;
 import com.miltonlearn.call.UserCall;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 //@Component(value = "userService")
-//@Service(value = "userService")
 //@Repository(value = "userService")
-@Controller(value = "userService")
+//@Controller(value = "userService")
+@Service(value = "userService")
 public class UserService {
+  @Autowired // byType
+  @Qualifier(value = "userCallObj") // byName
   private UserCall userCall;
 
   public void setUserCall(UserCall userCall) {
