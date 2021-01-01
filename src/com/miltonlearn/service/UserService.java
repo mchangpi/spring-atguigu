d430315dd89a34c12c23c2161b09a05cab134077
Milton Chang <mchangpi@gmail.com>
2021-01-01 20:01:14 +0800

    P23. Annotation Config - @Resource, @Value

--- src/com/miltonlearn/service/UserService.java
+++ src/com/miltonlearn/service/UserService.java
@@ -1,12 +1,9 @@
 package com.miltonlearn.service;
 
-import com.miltonlearn.call.IUserCall;
 import com.miltonlearn.call.UserCall;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Controller;
-import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 //@Component(value = "userService")
@@ -14,16 +11,26 @@ import org.springframework.stereotype.Service;
 //@Controller(value = "userService")
 @Service(value = "userService")
 public class UserService {
+
+  /* Injection for primitive types */
+  @Value(value = "guestService")
+  private String name;
+
+  /* @Autowired @Qualifer are used together */
   @Autowired // byType
   @Qualifier(value = "userCallObj") // byName
   private UserCall userCall;
+
+  /*
+  @Resource(name = "userCallObj")
+  private UserCall userCall;*/
 
   public void setUserCall(UserCall userCall) {
     this.userCall = userCall;
   }
 
   public void add() {
-    System.out.println("Service add...");
+    System.out.println("Service add name " + name);
     if (null != userCall)
       userCall.update();
   }
