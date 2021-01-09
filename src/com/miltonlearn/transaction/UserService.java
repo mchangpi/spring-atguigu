cf39eef409d2e86a3d3a85d9710050fedca76190
Milton Chang <mchangpi@gmail.com>
2021-01-09 14:19:15 +0800

    P45. Transaction - @Transactional(propagation = Propagation.REQUIRED)

--- src/com/miltonlearn/transaction/UserService.java
+++ src/com/miltonlearn/transaction/UserService.java
@@ -2,10 +2,11 @@ package com.miltonlearn.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(propagation = Propagation.REQUIRED)
 public class UserService {
   @Autowired
   private UserCall userCall;
