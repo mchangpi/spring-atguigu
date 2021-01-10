10b72c7f85889f7475b84baf0ef49cf05dcc1197
Milton Chang <mchangpi@gmail.com>
2021-01-10 20:49:25 +0800

    P46. Transaction - isolation = Isolation.REPEATABLE_READ

--- src/com/miltonlearn/transaction/UserService.java
+++ src/com/miltonlearn/transaction/UserService.java
@@ -2,11 +2,12 @@ package com.miltonlearn.transaction;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional(propagation = Propagation.REQUIRED)
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class UserService {
   @Autowired
   private UserCall userCall;
